e4a626230194f5db344d880915986080351d962b
Neelmani Bhrdwaj <neelmanibhardwaj001@gmail.com>
2024-09-03 07:44:23 -0400

    Adding js file

---
+++ script.js
@@ -0,0 +1,6 @@
+
+        var typed = new Typed('#element', {
+            strings: ['Computer Science Student', 'Web developer', 'Software engineer', 'Robotics Technician'],
+            typeSpeed: 100,
+        });
+    
